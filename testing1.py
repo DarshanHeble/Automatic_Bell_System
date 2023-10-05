6209f732778e0678d449d70867592c6d79643247
Darshan Heble <darshanheble@gmail.com>
2023-10-06 00:40:58 +0530

    .

--- testing1.py
+++ testing1.py
@@ -1,13 +1,21 @@
-import tkinter as tk
-from tkinter import ttk
+from customtkinter import *
+from PIL import Image
 
-root = tk.Tk()
-root.geometry("500x500")
+app = CTk()
+app.geometry("500x400")
+set_appearance_mode("")
 
-style = ttk.Style()
-style.configure("Large.Tbutton", font=("arial", 20))
+btn = CTkButton(
+    master=app,
+    text="add alarm",
+    corner_radius=32,
+    # fg_color="#C850C0",
+    fg_color="transparent",
+    hover_color="#4158D0",
+    border_color="#FFCC70",
+    border_width=2,
+)
+# btn.pack(relx=0.5, rely=0.5, anchor="center")
+btn.place(relx=0.5, rely=0.5, anchor="center")
 
-add_bell_button = ttk.Button(root, text="button", style="Large.Tbutton")
-add_bell_button.pack(anchor="ne")
-
-root.mainloop()
+app.mainloop()
