a8ce813359503d353d2e487626aeef0b231dda42
Darshan Heble <darshanheble@gmail.com>
2024-03-17 00:29:45 +0530

    .......

--- src/app/component/pages/announcement/announcement.component.ts
+++ src/app/component/pages/announcement/announcement.component.ts
@@ -4,6 +4,7 @@ import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { MatMenuModule } from '@angular/material/menu';
 @Component({
   selector: 'app-announcement',
   standalone: true,
@@ -15,6 +16,7 @@ import { MatInputModule } from '@angular/material/input';
     MatFormFieldModule,
     MatDialogModule,
     MatInputModule,
+    MatMenuModule,
   ],
 })
 export class AnnouncementComponent {}
