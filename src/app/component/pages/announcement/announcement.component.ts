cfbac7a12fccc24d50f7df39a059bfc46f5ecf25
Darshan Heble <darshanheble@gmail.com>
2024-03-17 01:23:16 +0530

    UI improvement

--- src/app/component/pages/announcement/announcement.component.ts
+++ src/app/component/pages/announcement/announcement.component.ts
@@ -1,22 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatChipsModule } from '@angular/material/chips';
 import { MatDialogModule } from '@angular/material/dialog';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatMenuModule } from '@angular/material/menu';
+
 @Component({
   selector: 'app-announcement',
   standalone: true,
   templateUrl: './announcement.component.html',
   styleUrl: './announcement.component.scss',
   imports: [
+    CommonModule,
     MatIconModule,
     MatButtonModule,
     MatFormFieldModule,
     MatDialogModule,
     MatInputModule,
     MatMenuModule,
+    MatChipsModule,
   ],
 })
 export class AnnouncementComponent {}
