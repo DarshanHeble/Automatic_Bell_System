2facf3dcd5993858978443d613aa6a28a9a1ef5e
Darshan Heble <darshanheble@gmail.com>
2024-03-18 01:30:02 +0530

    better time input experience

--- src/app/component/pages/announcement/announcement.component.ts
+++ src/app/component/pages/announcement/announcement.component.ts
@@ -24,4 +24,56 @@ import { MatMenuModule } from '@angular/material/menu';
     MatChipsModule,
   ],
 })
-export class AnnouncementComponent {}
+export class AnnouncementComponent {
+  hr_btn_text: number = 1;
+  min_btn_text: number = 0;
+  active_am_or_pm: string = 'am';
+
+  chips = [
+    { day: 'sun', active: false },
+    { day: 'mon', active: false },
+    { day: 'tue', active: false },
+    { day: 'wed', active: false },
+    { day: 'thu', active: false },
+    { day: 'fri', active: false },
+    { day: 'sat', active: false },
+  ];
+  on_hr_scroll(event: WheelEvent, btn: any): void {
+    if (event.deltaY > 0) {
+      // down wheel
+      if (this.hr_btn_text == 12) {
+        this.hr_btn_text = 1;
+      }
+      this.hr_btn_text++;
+    } else if (event.deltaY < 0) {
+      // Up wheel
+      if (this.hr_btn_text == 1) {
+        this.hr_btn_text = 12;
+      }
+      this.hr_btn_text--;
+    }
+  }
+  on_min_scroll(event: WheelEvent, btn: any): void {
+    if (event.deltaY > 0) {
+      // down wheel
+      if (this.min_btn_text == 59) {
+        this.min_btn_text = 0;
+      }
+      this.min_btn_text++;
+    } else if (event.deltaY < 0) {
+      // Up wheel
+      if (this.min_btn_text == 0) {
+        this.min_btn_text = 59;
+      }
+      this.min_btn_text--;
+    }
+  }
+  set_ampm_active(item: any) {
+    if (item !== this.active_am_or_pm) {
+      this.active_am_or_pm = item;
+    }
+  }
+  toogle(item: any) {
+    item.active = !item.active;
+  }
+}
